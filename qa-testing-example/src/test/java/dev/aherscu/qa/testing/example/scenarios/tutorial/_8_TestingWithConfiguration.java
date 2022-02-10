3e51cc89aacee957856aaf370b253308fa65dee9
Adrian Herscu <adrian.herscu@philips.com>
2022-02-10 13:26:25 +0200

    enhanced report

--- qa-testing-example/src/test/java/dev/aherscu/qa/testing/example/scenarios/tutorial/_8_TestingWithConfiguration.java
+++ qa-testing-example/src/test/java/dev/aherscu/qa/testing/example/scenarios/tutorial/_8_TestingWithConfiguration.java
@@ -17,7 +17,6 @@
 package dev.aherscu.qa.testing.example.scenarios.tutorial;
 
 import static io.appium.java_client.MobileBy.*;
-import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
 import org.hamcrest.*;
@@ -30,6 +29,7 @@ import dev.aherscu.qa.jgiven.commons.fixtures.*;
 import dev.aherscu.qa.jgiven.commons.utils.*;
 import dev.aherscu.qa.jgiven.commons.verifications.*;
 import dev.aherscu.qa.testing.example.*;
+import lombok.*;
 
 public class _8_TestingWithConfiguration
     // NOTE: just to get the configuration from the framework
@@ -44,32 +44,14 @@ public class _8_TestingWithConfiguration
     @ScenarioStage
     protected CalculatorVerifications<?> calculatorVerifications;
 
-    static public class CalculatorFixtures<SELF extends CalculatorFixtures<SELF>>
-        extends WebDriverFixtures<SELF> {
-        SELF a_calculator(final WebDriverEx driver) {
-            return a_web_driver(driver);
-        }
-    }
-
-    static public class CalculatorActions<SELF extends CalculatorActions<SELF>>
-        extends WebDriverActions<SELF> {
-        SELF typing(final String value) {
-            return typing_$_into(value, AccessibilityId("CalculatorResults"));
-        }
-    }
-
-    static public class CalculatorVerifications<SELF extends CalculatorVerifications<SELF>>
-        extends WebDriverVerifications<SELF> {
-        SELF the_title(final Matcher<String> matcher) {
-            assertThat(webDriver.get().asWindows().getTitle(), matcher);
-            return self();
-        }
-
-        SELF the_result(final Matcher<String> matcher) {
-            return eventually_assert_that(
-                () -> element(AccessibilityId("CalculatorResults")).getText(),
-                matcher);
-        }
+    @DataProvider
+    static Object[][] data() {
+        return new Object[][] {
+            { Calculation.builder().expression("7+8").result("15").build() },
+            { Calculation.builder().expression("2*5").result("11").build() },
+            { Calculation.builder().expression("333/111").result("3").build() },
+            { Calculation.builder().expression("7-8").result("-1").build() },
+        };
     }
 
     @Test
@@ -82,24 +64,47 @@ public class _8_TestingWithConfiguration
     }
 
     @Test(dataProvider = INTERNAL_DATA_PROVIDER)
-    public void shouldCalculate(final String expression, final String result) {
+    public void shouldCalculate(final Calculation calculation) {
         calculatorFixtures
             .given().a_calculator(webDriver.get());
 
         calculatorActions
-            .when().typing(expression + "=");
+            .when().typing(calculation.expression + "=");
 
         calculatorVerifications
-            .then().the_result(is(stringContainsInOrder("Display is", result)));
+            .then().the_result(is(
+                stringContainsInOrder("Display is", calculation.result)));
     }
 
-    @DataProvider
-    static Object[][] data() {
-        return new Object[][] {
-            { "7+8", "15" },
-            { "7-8", "-1" },
-            { "2*5", "11" },
-            { "6/2", "3" },
-        };
+    static public class CalculatorFixtures<SELF extends CalculatorFixtures<SELF>>
+        extends WebDriverFixtures<SELF> {
+        SELF a_calculator(final WebDriverEx driver) {
+            return a_web_driver(driver);
+        }
+    }
+
+    static public class CalculatorActions<SELF extends CalculatorActions<SELF>>
+        extends WebDriverActions<SELF> {
+        SELF typing(final String expression) {
+            return typing_$_into(expression,
+                AccessibilityId("CalculatorResults"));
+        }
+    }
+
+    static public class CalculatorVerifications<SELF extends CalculatorVerifications<SELF>>
+        extends WebDriverVerifications<SELF> {
+
+        @AttachesScreenshot
+        SELF the_result(final Matcher<String> matcher) {
+            return eventually_assert_that(
+                () -> element(AccessibilityId("CalculatorResults")).getText(),
+                matcher);
+        }
+    }
+
+    @Builder
+    @ToString
+    static class Calculation {
+        final String expression, result;
     }
 }
