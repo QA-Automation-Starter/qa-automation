b926c1d83e455f8c431e354eb5e459355020af2b
Adrian Herscu <adrian.herscu@philips.com>
2022-02-10 11:15:25 +0200

    rewritten to work with jgiven

--- qa-testing-example/src/test/java/dev/aherscu/qa/testing/example/scenarios/tutorial/_8_TestingWithConfiguration.java
+++ qa-testing-example/src/test/java/dev/aherscu/qa/testing/example/scenarios/tutorial/_8_TestingWithConfiguration.java
@@ -16,31 +16,81 @@
 
 package dev.aherscu.qa.testing.example.scenarios.tutorial;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.stringContainsInOrder;
+import static io.appium.java_client.MobileBy.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
 
+import org.hamcrest.*;
 import org.testng.annotations.*;
 
+import com.tngtech.jgiven.annotation.*;
+
+import dev.aherscu.qa.jgiven.commons.actions.*;
+import dev.aherscu.qa.jgiven.commons.fixtures.*;
+import dev.aherscu.qa.jgiven.commons.utils.*;
+import dev.aherscu.qa.jgiven.commons.verifications.*;
 import dev.aherscu.qa.testing.example.*;
 
 public class _8_TestingWithConfiguration
+    // NOTE: just to get the configuration from the framework
     extends ApplicationPerClassWebSessionTest {
+
+    @ScenarioStage
+    protected CalculatorFixtures<?>      calculatorFixtures;
+
+    @ScenarioStage
+    protected CalculatorActions<?>       calculatorActions;
+
+    @ScenarioStage
+    protected CalculatorVerifications<?> calculatorVerifications;
+
+    static public class CalculatorFixtures<SELF extends CalculatorFixtures<SELF>>
+        extends WebDriverFixtures<SELF> {
+        SELF a_calculator(final WebDriverEx driver) {
+            return a_web_driver(driver);
+        }
+    }
+
+    static public class CalculatorActions<SELF extends CalculatorActions<SELF>>
+        extends WebDriverActions<SELF> {
+        SELF typing(final String value) {
+            return typing_$_into(value, AccessibilityId("CalculatorResults"));
+        }
+    }
+
+    static public class CalculatorVerifications<SELF extends CalculatorVerifications<SELF>>
+        extends WebDriverVerifications<SELF> {
+        SELF the_title(final Matcher<String> matcher) {
+            assertThat(webDriver.get().asWindows().getTitle(), matcher);
+            return self();
+        }
+
+        SELF the_result(final Matcher<String> matcher) {
+            return eventually_assert_that(
+                () -> element(AccessibilityId("CalculatorResults")).getText(),
+                matcher);
+        }
+    }
+
     @Test
     public void shouldOpenCalculator() {
-        assertThat(webDriver.get().asWindows().getTitle(),
-            equalTo("Calculator"));
+        calculatorFixtures
+            .given().a_calculator(webDriver.get());
+
+        calculatorVerifications
+            .then().the_title(is(equalTo("Calculator")));
     }
 
     @Test(dataProvider = INTERNAL_DATA_PROVIDER)
     public void shouldCalculate(final String expression, final String result) {
-        webDriver.get().asWindows()
-            .findElementByAccessibilityId("CalculatorResults")
-            .sendKeys(expression + "=");
-        assertThat(webDriver.get().asWindows()
-            .findElementByAccessibilityId("CalculatorResults")
-            .getText(),
-            stringContainsInOrder("Display is", result));
+        calculatorFixtures
+            .given().a_calculator(webDriver.get());
+
+        calculatorActions
+            .when().typing(expression + "=");
+
+        calculatorVerifications
+            .then().the_result(is(stringContainsInOrder("Display is", result)));
     }
 
     @DataProvider
