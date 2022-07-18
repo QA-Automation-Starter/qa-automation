3818709f870fcbf45a251a6ab742ff6d503b7098
Adrian Herscu <adrian.herscu@gmail.com>
2022-07-18 09:28:07 +0300

    documented required maven settings for use with ossrh and fixed to work with unknown ssl certificates

--- qa-testing-archetype/generate-example-project.bat
+++ qa-testing-archetype/generate-example-project.bat
@@ -1,8 +1,9 @@
-mkdir demo
-chdir demo
-mvn archetype:generate ^
+mvn --batch-mode archetype:generate ^
+  -Dmaven.wagon.http.ssl.insecure=true ^
   -DarchetypeGroupId=dev.aherscu.qa ^
   -DarchetypeArtifactId=qa-testing-archetype ^
   -DarchetypeVersion=0.0.1-SNAPSHOT ^
   -DgroupId=com.acme ^
-  -DartifactId=testing
+  -DartifactId=testing ^
+  -Dversion=1.0-SNAPSHOT ^
+  -Dpackage=com.acme.testing
