b6472f4c703cedf25a8a7154712dea71775a2f7b
Adrian Herscu <adrian.herscu@gmail.com>
2022-08-29 22:49:39 +0300

    added google search test example

---
+++ qa-testing-archetype/src/main/resources/archetype-resources/src/main/java/steps.tutorial/GoogleFixtures.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2022 Adrian Herscu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ${package}.steps.tutorial;
+
+import dev.aherscu.qa.jgiven.commons.fixtures.*;
+import dev.aherscu.qa.jgiven.commons.utils.*;
+
+public class GoogleFixtures<SELF extends GoogleFixtures<SELF>>
+    extends WebDriverFixtures<SELF> {
+    public SELF google(final WebDriverEx driver) {
+        return a_web_driver(driver)
+            .at("https://google.com");
+    }
+}
---
+++ qa-testing-archetype/src/main/resources/archetype-resources/src/main/java/steps.tutorial/GoogleVerifications.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2022 Adrian Herscu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ${package}.steps.tutorial;
+
+import java.util.stream.*;
+
+import org.hamcrest.*;
+import org.openqa.selenium.*;
+
+import dev.aherscu.qa.jgiven.commons.model.*;
+import dev.aherscu.qa.jgiven.commons.utils.*;
+import dev.aherscu.qa.jgiven.commons.verifications.*;
+import ${package}.model.tutorial.*;
+import lombok.extern.slf4j.*;
+
+@Slf4j
+public class GoogleVerifications<SELF extends GoogleVerifications<SELF>>
+    extends WebDriverVerifications<SELF> {
+
+    @AttachesScreenshot
+    public SELF the_results(final Matcher<Stream<GoogleResult>> matcher) {
+        return eventually_assert_that(
+            () -> elements(By.xpath("//a/h3"))
+                .stream()
+                .map(resultElement -> GoogleResult.builder()
+                    .title(new Text(resultElement
+                        .getAttribute("textContent")))
+                    // populate other fields per need
+                    .build()),
+            matcher);
+    }
+}
