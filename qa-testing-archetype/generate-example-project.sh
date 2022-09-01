278ad39308ecfab09045eb2c2833e3162dba55c8
Adrian Herscu <adrian.herscu@gmail.com>
2022-09-01 16:32:55 +0300

    trying with explicit archetype version

--- qa-testing-archetype/generate-example-project.sh
+++ qa-testing-archetype/generate-example-project.sh
@@ -5,7 +5,6 @@ mvn archetype:generate \
   -Dmaven.wagon.http.ssl.insecure=true \
   -DarchetypeGroupId=dev.aherscu.qa \
   -DarchetypeArtifactId=qa-testing-archetype \
-  -DarchetypeVersion=0.0.2-SNAPSHOT \
   -DgroupId=com.acme \
   -DartifactId=testing \
   -Dversion=1.0-SNAPSHOT \
