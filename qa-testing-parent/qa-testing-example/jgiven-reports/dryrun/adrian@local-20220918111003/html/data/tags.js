89a15f64145e45fc934550279dbdd23c8a7918a0
Adrian Herscu <adrian.herscu@gmail.com>
2022-09-18 14:22:01 +0300

    Site checkin for project QA Automation

---
+++ qa-testing-parent/qa-testing-example/jgiven-reports/dryrun/adrian@local-20220918111003/html/data/tags.js
@@ -0,0 +1,1 @@
+jgivenReport.setTags({"tagTypeMap":{"dev.aherscu.qa.jgiven.commons.tags.UITest":{"fullType":"dev.aherscu.qa.jgiven.commons.tags.UITest","type":"UITest","name":"UI Test","description":"Tests interacting with user interface","href":""}},"tags":{"dev.aherscu.qa.jgiven.commons.tags.UITest":{"tagType":"dev.aherscu.qa.jgiven.commons.tags.UITest"}}} );
