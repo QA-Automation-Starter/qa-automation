c39e15cebecd5fdd33ee79d1ff9b03b2413267f8
Adrian Herscu <adrian.herscu@gmail.com>
2022-11-25 21:13:29 +0200

    fixed examples for running windows and for running tutorials from standalone jar

--- qa-testing-example/standalone-tutorials.bat
+++ qa-testing-example/standalone-tutorials.bat
@@ -1,11 +1,11 @@
 java ^
--Dlog.root.level=trace ^
+-Dlog.root.level=info ^
 -Dlogback.configurationFile=logback-test.xml ^
 -Dpoll.timeout=15 ^
 -Dpoll.delay=5 ^
 -Dsaucelabs.reporter.url= ^
--Dprovider= ^
--Ddevice.type=windows ^
+-Dprovider=provider.local. ^
+-Ddevice.type= ^
 -Dbuild.label=STANDALONE ^
 -Dbuild.tags= ^
 -Dapplication.filename=Microsoft.WindowsCalculator_8wekyb3d8bbwe!App ^
@@ -15,4 +15,4 @@ java ^
 -Ddryrun= ^
 -jar target/qa-testing-example-0.0.9-SNAPSHOT-test-with-dependencies.jar ^
 -d target/test-output ^
-testing-windows.xml
+testing-tutorials.xml
