28ab0acbe281e4c1dd46678aa1277d4922578d72
Adrian Herscu <adrian.herscu@gmail.com>
2022-11-25 21:16:07 +0200

    fixed archetype for running windows and for running tutorials from standalone jar

---
+++ qa-testing-archetype/src/main/resources/archetype-resources/standalone-tutorials.bat
@@ -0,0 +1,18 @@
+java ^
+-Dlog.root.level=info ^
+-Dlogback.configurationFile=logback-test.xml ^
+-Dpoll.timeout=15 ^
+-Dpoll.delay=5 ^
+-Dsaucelabs.reporter.url= ^
+-Dprovider=provider.local. ^
+-Ddevice.type= ^
+-Dbuild.label=STANDALONE ^
+-Dbuild.tags= ^
+-Dapplication.filename=Microsoft.WindowsCalculator_8wekyb3d8bbwe!App ^
+-Dapplication.workingdir= ^
+-Dtest.properties.file=environments/default/test.properties ^
+-Djgiven.report.dir=target/jgiven-reports ^
+-Ddryrun= ^
+-jar target/${artifactId}-${version}-test-with-dependencies.jar ^
+-d target/test-output ^
+testing-tutorials.xml
