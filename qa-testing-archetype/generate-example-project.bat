69cdf6bf0f442b48fa044cd3b3be71e7cdc47bc7
Adrian Herscu <adrian.herscu@gmail.com>
2022-11-26 12:53:25 +0200

    could not make it use latest snapshot; opened stackoverflow question

--- qa-testing-archetype/generate-example-project.bat
+++ qa-testing-archetype/generate-example-project.bat
@@ -1,11 +1,18 @@
-mvn archetype:generate ^
-  --batch-mode ^
+@echo off
+pushd target
+@rem ISSUE using LATEST for archetypeVersion always resolves to latest release
+@rem instead of latest snapshot
+@rem https://stackoverflow.com/questions/74581510/using-latest-for-archetypeversion-always-resolves-to-latest-release
+call mvn archetype:generate ^
+  --show-version ^
   --settings ..\development-maven-settings.xml ^
   -Dmaven.wagon.http.ssl.insecure=true ^
+  -DarchetypeCatalog=local ^
   -DarchetypeGroupId=dev.aherscu.qa ^
   -DarchetypeArtifactId=qa-testing-archetype ^
-  -DarchetypeVersion=LATEST ^
+  -DarchetypeVersion=0.0.9-SNAPSHOT ^
   -DgroupId=com.acme ^
   -DartifactId=testing ^
   -Dversion=0.0.1-SNAPSHOT ^
   -Dpackage=com.acme.testing
+popd
