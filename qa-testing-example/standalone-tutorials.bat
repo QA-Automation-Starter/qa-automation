569e1ee9b133ecefaeaa9021a5665041f442394f
Adrian Herscu <adrian.herscu@gmail.com>
2022-12-09 19:24:21 +0200

    added required system properties for taking screenshots

--- qa-testing-example/standalone-tutorials.bat
+++ qa-testing-example/standalone-tutorials.bat
@@ -12,7 +12,9 @@ java ^
 -Dapplication.workingdir= ^
 -Dtest.properties.file=environments/default/test.properties ^
 -Djgiven.report.dir=target/jgiven-reports ^
--Ddryrun= ^
+-Ddryrun=false ^
+-Dscreenshots=true ^
+-DscreenshotDelayMs=500 ^
 -jar target/qa-testing-example-0.0.9-SNAPSHOT-test-with-dependencies.jar ^
 -listener dev.aherscu.qa.jgiven.reporter.QaJGivenPerMethodReporter ^
 -d target/test-output ^
