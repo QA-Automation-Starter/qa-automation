6dc9d83984586fae40d94585cdf447ec9f674c12
Adrian Herscu <adrian.herscu@gmail.com>
2022-12-20 05:50:35 +0000

    Site checkin for project QA Automation

--- qa-testing-parent/qa-testing-example/jgiven-reports/functional-default/runneradmin@local-20221211181622/html/data/metaData.js
+++
@@ -1,1 +0,0 @@
-jgivenReport.setMetaData({"created":"Dec 11, 2022 6:26:58 PM","version":"1.1.0-365403fc","title":"QA Testing Example  [#runneradmin@local-20221211181622]","data":["data0.js"],"showThumbnails":true} );
