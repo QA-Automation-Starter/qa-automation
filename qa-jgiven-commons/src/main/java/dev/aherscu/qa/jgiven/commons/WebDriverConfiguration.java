ff358cb5d447039e223c2a346619d8b045adca4b
Adrian Herscu <adrian.herscu@gmail.com>
2023-02-12 22:01:14 +0200

    fixed default device.type property to empty string

--- qa-jgiven-commons/src/main/java/dev/aherscu/qa/jgiven/commons/WebDriverConfiguration.java
+++ qa-jgiven-commons/src/main/java/dev/aherscu/qa/jgiven/commons/WebDriverConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Adrian Herscu
+ * Copyright 2023 Adrian Herscu
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -150,7 +150,7 @@ public class WebDriverConfiguration extends BaseConfiguration {
      *         {@link Platform#ANY} if none specified
      */
     public DeviceType deviceType() {
-        return DeviceType.from(getString("device.type"));
+        return DeviceType.from(getString("device.type", EMPTY));
     }
 
     /**
