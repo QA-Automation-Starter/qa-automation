a6fe88fed60eaf4ec80da1e6d241803adc8a712a
Adrian Herscu <adrian.herscu@gmail.com>
2023-07-29 19:19:36 +0300

    added note to load saucelabs credentials from system environment instead of having them hardcoded

--- qa-testing-example/src/test/java/dev/aherscu/qa/testing/example/scenarios/tutorial4/TestingAndroidOnSauceLabs.java
+++ qa-testing-example/src/test/java/dev/aherscu/qa/testing/example/scenarios/tutorial4/TestingAndroidOnSauceLabs.java
@@ -46,6 +46,8 @@ public class TestingAndroidOnSauceLabs {
     static AndroidDriver<WebElement> saucelabsApp(
         final String name) {
         return new AndroidDriver<>(new URL(
+            // FIXME should get these credentials from system enviroment
+            //  SAUCELABS_USERNAME and SAUCELABS_PASSWORD
             "https://TBD:TBD@ondemand.saucelabs.com:443/wd/hub"),
             new DesiredCapabilities() {
                 {
