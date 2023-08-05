9e54bfc0aab80b3762ae00448a1640ce3318cd7a
Adrian Herscu <adrian.herscu@gmail.com>
2023-08-05 18:48:09 +0300

    separated tests and added verification by expectation id

--- qa-testing-example/src/test/java/dev/aherscu/qa/testing/example/scenarios/tutorial6/GenericMockServerRestTest.java
+++ qa-testing-example/src/test/java/dev/aherscu/qa/testing/example/scenarios/tutorial6/GenericMockServerRestTest.java
@@ -15,6 +15,7 @@
  */
 package dev.aherscu.qa.testing.example.scenarios.tutorial6;
 
+import static dev.aherscu.qa.testing.utils.StringUtilsExtensions.*;
 import static java.util.Arrays.*;
 import static org.hamcrest.Matchers.*;
 import static org.mockserver.model.HttpError.*;
@@ -22,8 +23,12 @@ import static org.mockserver.model.HttpRequest.*;
 import static org.mockserver.model.HttpResponse.*;
 import static org.mockserver.model.MediaType.*;
 
+import java.util.*;
+
 import javax.ws.rs.client.*;
 
+import org.mockserver.mock.*;
+import org.mockserver.model.*;
 import org.testng.annotations.*;
 
 import dev.aherscu.qa.jgiven.commons.tags.*;
@@ -51,15 +56,38 @@ import lombok.*;
 public final class GenericMockServerRestTest extends
     AbstractMockServerTest<RestScenarioType, RestFixtures<?>, RestActions<?>, RestVerifications<?>> {
 
-    private Client client;
+    private Client        client;
 
-    /**
-     * Should retrieve a JSON field from a REST service
-     */
+    private Expectation[] expectations;
+
+    private static ExpectationId[] as(final Expectation[] expectations) {
+        return Arrays.stream(expectations)
+            .map(Expectation::getId)
+            .map(ExpectationId::expectationId)
+            .toArray(ExpectationId[]::new);
+    }
+
     @Test
     @Reference("159")
     @SneakyThrows
-    public void shouldRetrieveJsonFieldFromFakeRestService() {
+    public void shouldVerifyAccessByExpectionId() {
+        given()
+            .a_REST_client(client);
+
+        when()
+            .connecting_to(mockServerUri())
+            .and().appending_path("dummy")
+            .and().getting_the_response();
+
+        then()
+            .$("the dummy request was sent",
+                __ -> mockServer.verify(as(expectations)));
+    }
+
+    @Test
+    @Reference("159")
+    @SneakyThrows
+    public void shouldVerifyAccessByDefinition() {
         given()
             .a_REST_client(client);
 
@@ -72,9 +100,16 @@ public final class GenericMockServerRestTest extends
             .the_response_contents(asList(
                 new JsonAssertion<>("$[0].id", greaterThan(0)),
                 new JsonAssertion<>("$[1].id", equalTo(2))))
-            .and().$("the requests were sent to mock server",
-                __ -> mockServer.verify(request()
-                    .withPath("/some-id")));
+            .and().$("the some id request was sent",
+                __ -> mockServer.verify(request().withPath("/some-id")));
+    }
+
+    @Test
+    @Reference("159")
+    @SneakyThrows
+    public void shouldVeryAccessOfDroppedConnection() {
+        given()
+            .a_REST_client(client);
 
         when()
             .connecting_to(mockServerUri())
@@ -83,7 +118,7 @@ public final class GenericMockServerRestTest extends
                 self -> self.safely(__ -> self.getting_the_response()));
 
         then()
-            .$("the requests were sent to mock server",
+            .$("the drop connection request was sent",
                 __ -> mockServer.verify(request()
                     .withPath("/drop-connection")));
     }
@@ -95,14 +130,16 @@ public final class GenericMockServerRestTest extends
 
     @BeforeClass
     private void beforeClassAddExpectations() {
-        mockServer
-            .when(request() // GET is implied
-                .withPath("/drop-connection"))
+        expectations = mockServer // GET is implied
+            .when(request().withPath("/dummy"))
+            .respond(response(EMPTY));
+
+        mockServer // GET is implied
+            .when(request().withPath("/drop-connection"))
             .error(error().withDropConnection(true));
 
-        mockServer
-            .when(request() // GET is implied
-                .withPath("/some-id"))
+        mockServer // GET is implied
+            .when(request().withPath("/some-id"))
             .respond(response()
                 .withBody("[{\"id\":1},{\"id\":2},{\"id\":3}]", JSON_UTF_8));
     }
