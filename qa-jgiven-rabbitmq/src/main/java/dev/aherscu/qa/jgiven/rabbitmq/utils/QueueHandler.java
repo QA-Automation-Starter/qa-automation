7e6ce22db1e10d9c3d9981bc13ab4f6914a80d49
Adrian Herscu <aherscu@crossriver.com>
2023-10-02 14:00:57 -0400

    added a singleton with self-managed connection

--- qa-jgiven-rabbitmq/src/main/java/dev/aherscu/qa/jgiven/rabbitmq/utils/QueueHandler.java
+++ qa-jgiven-rabbitmq/src/main/java/dev/aherscu/qa/jgiven/rabbitmq/utils/QueueHandler.java
@@ -16,6 +16,8 @@
 
 package dev.aherscu.qa.jgiven.rabbitmq.utils;
 
+import static com.google.common.base.Suppliers.*;
+import static java.lang.Runtime.*;
 import static java.util.Collections.*;
 import static java.util.Objects.*;
 
@@ -66,6 +68,97 @@ import lombok.extern.slf4j.*;
 @SuperBuilder
 @Slf4j
 public class QueueHandler<K, V> implements AutoCloseable {
+    /**
+     * Utility for building a singleton QueueHandler.
+     *
+     * <p>
+     * Build it:
+     * 
+     * <pre>
+     * private final Supplier<QueueHandler<YourKeyType, YourContentType>> queueHandlerSupplier =
+     *     QueueHandler.Singleton.<UUID, RetrieveTransactionResponse> builder()
+     *         .queue(queueName())
+     *         .connectionFactory(amqpConnectionFactory())
+     *         .indexingBy(message -> message.content.yourKey)
+     *         .consumingBy(
+     *             Unchecked.function(YourContentType::deserializeFromBytes))
+     *         .publishingBy(YourContentType::serializeToBytes)
+     *         .build()
+     *         .supplier();
+     * </pre>
+     *
+     * and, later:
+     *
+     * <pre>
+     * public QueueHandler<YourKeyType, YourContentType> queueHandler() {
+     *     return queueHandlerSupplier.get();
+     * }
+     * </pre>
+     *
+     * It is guaranteed that calling {@code queueHandler()} <strong>from any
+     * thread</strong>, will supply the same QueueHandler instance.
+     * </p>
+     * 
+     * <p>
+     * The connection is internally managed and closed via a runtime shutdown
+     * hook.
+     * </p>
+     * 
+     * @param <K>
+     *            type of message-key; it should have a proper hash function in
+     *            order to get O(1) access time, otherwise it may degrade to
+     *            O(n)
+     * @param <V>
+     *            type of message-value
+     */
+    @Builder
+    public static final class Singleton<K, V> {
+        public final ConnectionFactory       connectionFactory;
+        public final String                  queue;
+        public final Function<Message<V>, K> indexingBy;
+        public final Function<byte[], V>     consumingBy;
+        public final Function<V, byte[]>     publishingBy;
+
+        /**
+         * @return a memoized supplier of QueueHandler
+         */
+        public Supplier<QueueHandler<K, V>> supplier() {
+            return memoize(() -> {
+                final Connection connection;
+                final QueueHandler<K, V> queueHandler;
+                try {
+                    log.debug("creating connection");
+                    connection = connectionFactory.newConnection();
+                    log.debug("setting-up queue handler");
+                    queueHandler = QueueHandler.<K, V> builder()
+                        .channel(connection.createChannel())
+                        .queue(queue)
+                        .indexingBy(indexingBy)
+                        .consumingBy(consumingBy)
+                        .publishingBy(publishingBy)
+                        .build();
+                } catch (final IOException | TimeoutException e) {
+                    throw new RuntimeException(e);
+                }
+                getRuntime().addShutdownHook(new Thread(() -> {
+                    // NOTE: apparently the only way to purge unacked messages
+                    // is
+                    // via management interface
+                    // see
+                    // https://www.rabbitmq.com/amqp-0-9-1-reference.html#queue.purge
+                    try {
+                        log.debug("closing connection");
+                        connection.close();
+                    } catch (final IOException ioe) {
+                        log.warn("connection already closed {}",
+                            ioe.toString());
+                    }
+                }));
+                return queueHandler;
+            });
+        }
+    }
+
     public final Channel                       channel;
     public final String                        queue;
     public final Function<Message<V>, K>       indexingBy;
@@ -94,6 +187,33 @@ public class QueueHandler<K, V> implements AutoCloseable {
     }
 
     /**
+     * Cancels the consumption, previously started by {@link #consume()}. Must
+     * be called on same thread as last {@link #consume()} call.
+     *
+     * @throws IOException
+     *             upon connection failures, or if consumer was not started
+     * @return the consumer tag
+     */
+    public String cancel() throws IOException {
+        if (isNull(consumerTag)) {
+            throw new IOException("consumer not started");
+        }
+        log.debug("cancelling consumer by tag {}", consumerTag);
+        channel.basicCancel(consumerTag);
+        return consumerTag;
+    }
+
+    @Override
+    public void close() throws IOException {
+        try {
+            cancel();
+        } catch (final Exception e) {
+            log.error("while closing got {}", e.getMessage());
+            throw e;
+        }
+    }
+
+    /**
      * Starts the message consumption process and returns immediately.
      *
      * @return consumer tag
@@ -104,8 +224,8 @@ public class QueueHandler<K, V> implements AutoCloseable {
     @SneakyThrows
     public String consume() {
         channel.basicQos(16);
-        return consumerTag =
-            channel.basicConsume(queue, new DefaultConsumer(channel) {
+        consumerTag = channel.basicConsume(queue,
+            new DefaultConsumer(channel) {
                 @Override
                 public void handleDelivery(
                     @SuppressWarnings("hiding") final String consumerTag,
@@ -130,23 +250,22 @@ public class QueueHandler<K, V> implements AutoCloseable {
                     channel.basicAck(envelope.getDeliveryTag(), false);
                 }
             });
+
+        log.debug("set-up consumer with tag {}", consumerTag);
+        return consumerTag;
     }
 
-    /**
-     * Cancels the consumption, previously started by {@link #consume()}. Must
-     * be called on same thread as last {@link #consume()} call.
-     *
-     * @throws IOException
-     *             upon connection failures, or if consumer was not started
-     * @return the consumer tag
-     */
-    public String cancel() throws IOException {
-        if (isNull(consumerTag)) {
-            throw new IOException("consumer not started");
-        }
-        log.debug("cancelling {}", consumerTag);
-        channel.basicCancel(consumerTag);
-        return consumerTag;
+    public void publish(final Stream<Message<V>> messages) {
+        messages.parallel()
+            .peek(message -> log.trace("publishing {}", message))
+            .forEach(Unchecked.consumer(
+                message -> channel.basicPublish(StringUtils.EMPTY, queue,
+                    message.properties, publishingBy.apply(message.content))));
+    }
+
+    public void publishValues(final Stream<V> values) {
+        publish(values
+            .map(value -> Message.<V> builder().content(value).build()));
     }
 
     /**
@@ -156,27 +275,5 @@ public class QueueHandler<K, V> implements AutoCloseable {
      */
     public Map<K, Message<V>> recievedMessages() {
         return unmodifiableMap(recievedMessages);
-    }
-
-    @Override
-    public void close() throws IOException {
-        try {
-            cancel();
-        } catch (final Exception e) {
-            log.error("while closing got {}", e.getMessage());
-            throw e;
-        }
-    }
-
-    public void publish(final Stream<Message<V>> messages) {
-        messages.parallel()
-            .forEach(Unchecked.consumer(
-                message -> channel.basicPublish(StringUtils.EMPTY, queue,
-                    message.properties, publishingBy.apply(message.content))));
-    }
-
-    public void publishValues(final Stream<V> values) {
-        publish(values
-            .map(value -> Message.<V> builder().content(value).build()));
     }
 }
