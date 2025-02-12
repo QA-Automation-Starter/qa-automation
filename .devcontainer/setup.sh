dd5ba854e31695f6bb0cb8b64f6845d5489f9fa1
Adrian Herscu <adrian.herscu@gmail.com>
2025-02-12 22:11:21 +0000

    added mvn install to devcontainer's postcreate

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,3 +3,4 @@ set -e
 
 sudo chown -R vscode:vscode /workspaces/qa-automation
 sudo chown -R vscode:vscode /home/vscode/.m2
+cd /workspaces/qa-automation && ./mvnw install -Pmode-build-nosign,mode-aspectj-skip
