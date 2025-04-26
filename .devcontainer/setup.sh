e622e50aef9601cd209772b614555dc5d39c01fa
Adrian Herscu <adrian.herscu@gmail.com>
2025-04-26 16:46:42 +0000

    added mode-build-full

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,4 +3,4 @@ set -e
 
 sudo chown -R vscode:vscode /workspaces/qa-automation
 sudo chown -R vscode:vscode /home/vscode/.m2
-cd /workspaces/qa-automation && ./mvnw install -Pmode-build-nosign,mode-aspectj-skip
+cd /workspaces/qa-automation && ./mvnw install -Pmode-build-full,mode-build-nosign,mode-aspectj-skip
